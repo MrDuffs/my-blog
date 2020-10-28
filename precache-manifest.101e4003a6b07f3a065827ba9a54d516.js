719cb264ad39401c9a525d0e771291154f35b16f
MrDuffs <chulkov.i@yandex.ru>
2020-10-28 19:39:52 +0300

    Updates

---
+++ precache-manifest.101e4003a6b07f3a065827ba9a54d516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80bab1a34206705ac887856f07758ed",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "e3ad26afe48f26a73dd7",
+    "url": "/my-blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "249af11b0f116e48b376",
+    "url": "/my-blog/static/js/2.4c0a2322.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-blog/static/js/2.4c0a2322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ad26afe48f26a73dd7",
+    "url": "/my-blog/static/js/main.352f312a.chunk.js"
+  },
+  {
+    "revision": "36686c32f1b2062287aa",
+    "url": "/my-blog/static/js/runtime-main.10735d37.js"
+  }
+]);
